905137216f18fa95301d869c0f1f19c251537142
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2020-11-05 21:53:06 +0300

    Контрольная работа

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include "windows.h"
+#include <fstream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	HANDLE file1;
+	DWORD m;
+	int len = 0;
+	int size = 0;
+	char cBuffer[1026] = { 0 };
+	string text;
+	fstream file("passwd/test.txt");
+
+	setlocale(LC_ALL, "Russian");
+
+	file1 = CreateFile(L"CONIN$", GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+	ReadFile(file1, cBuffer, sizeof(cBuffer), &m, NULL);
+
+	text = cBuffer;
+	len = text.length() - 2;
+	cout << "Введённый текст весит: ";
+	if (len < 1024) cout << len << " байт" << endl;
+	else cout << 1024 << " байт" << endl;
+
+	file.seekg(0, ios::end);
+	size = file.tellg();
+	cout << "Файл весит: " << size << " байт" << endl;
+	file.close();
+
+	return 1;
+}
