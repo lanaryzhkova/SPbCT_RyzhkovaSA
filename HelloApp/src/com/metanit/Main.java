f85f4628601ee27aeed1f4b9b8e5100cb1251e57
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2022-01-15 23:36:18 +0300

    Java ПЗ_1

---
+++ HelloApp/src/com/metanit/Main.java
@@ -0,0 +1,13 @@
+package com.metanit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        double a = 3;
+        double b = 4;
+        double c;
+        c = Math.sqrt (a* a + b* b);
+        System.out.println ("c = "+ c);
+    }
+}
+
